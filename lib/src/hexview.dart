eb6bae9923b46b088d9cf01101620fca620308f9
eparisot <eparisot@student.42.fr>
2021-01-20 22:22:14 +0100

    added an ASCII representation at end of line, non printable chars are replaced by dots abstract class HexRecordFormatter renamed in abstract class AbsRecordFormatter for sake of clarity

--- lib/src/hexview.dart
+++ lib/src/hexview.dart
@@ -67,13 +67,14 @@ class Record {
   }
 }
 
-abstract class HexRecordFormatter {
+// Experimental : print ASCII repr
+abstract class AbsRecordFormatter {
   int get recordLength;
 
   String format(Record record, {int fullDataEndAddress});
 }
 
-class DefaultRecordFormatter implements HexRecordFormatter {
+class DefaultRecordFormatter implements AbsRecordFormatter {
   const DefaultRecordFormatter({this.recordLength = 16});
 
   @override
@@ -85,7 +86,7 @@ class DefaultRecordFormatter implements HexRecordFormatter {
     }
 
     final sb = StringBuffer();
-    sb.write('0x' + Hex.hex32(record.startAddress));
+    sb.write('0x' + Hex.hex16(record.startAddress));
     sb.write('\t');
     for (int datum in record.data) {
       if (datum != null) {
@@ -99,8 +100,38 @@ class DefaultRecordFormatter implements HexRecordFormatter {
   }
 }
 
+// Experimental : print ASCII repr
+class AsciiRecordFormatter implements AbsRecordFormatter {
+  const AsciiRecordFormatter({this.recordLength = 16});
+
+  @override
+  final int recordLength;
+
+  String format(Record record, {int fullDataEndAddress}) {
+    if (record.length != recordLength) {
+      throw Exception('Record should be of length $recordLength');
+    }
+
+    final sb = StringBuffer();
+    sb.write('\t\t');
+    for (int datum in record.data) {
+      if (datum == null) {
+        sb.write(' ');
+      } else if (datum > 32 && datum < 127) {
+        sb.write(String.fromCharCode(datum));
+      } else {
+        sb.write('.');
+      }
+      sb.write(' ');
+    }
+    return sb.toString();
+  }
+}
+
 String hexView(int startAddress, Iterable<int> data,
-    {HexRecordFormatter formatter = const DefaultRecordFormatter(),
+    {AbsRecordFormatter formatter = const DefaultRecordFormatter(),
+    bool printAscii = false,
+    AbsRecordFormatter asciiFormatter = const AsciiRecordFormatter(),
     String recordSeparator = '\n'}) {
   final recordLength = formatter.recordLength;
   final sb = StringBuffer();
@@ -113,6 +144,10 @@ String hexView(int startAddress, Iterable<int> data,
     data = data.skip(firstTake);
     final record = Record.prefix(startAddress, first, recordLength);
     sb.write(formatter.format(record));
+    // Experimental : print ASCII repr
+    if (printAscii) {
+      sb.write(asciiFormatter.format(record));
+    }
     address = record.startAddress + recordLength;
   }
 
@@ -121,6 +156,10 @@ String hexView(int startAddress, Iterable<int> data,
     final record = Record(address, recordData.toList());
     sb.write(recordSeparator);
     sb.write(formatter.format(record));
+    // Experimental : print ASCII repr
+    if (printAscii) {
+      sb.write(asciiFormatter.format(record));
+    }
     address = record.startAddress + recordLength;
     data = data.skip(recordLength);
   }
@@ -129,6 +168,10 @@ String hexView(int startAddress, Iterable<int> data,
     final record = Record.suffix(address, data, recordLength);
     sb.write(recordSeparator);
     sb.write(formatter.format(record));
+    // Experimental : print ASCII repr
+    if (printAscii) {
+      sb.write(asciiFormatter.format(record));
+    }
   }
 
   return sb.toString();
