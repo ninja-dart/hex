a38aa67fd2b7c48e79fd9fe0abb2ad18d0f8c089
eparisot <eparisot@student.42.fr>
2021-01-21 14:47:37 +0100

    clean and update hexView example
    Signed-off-by: eparisot <eparisot@student.42.fr>

--- example/hexview_example.dart
+++ example/hexview_example.dart
@@ -1,5 +1,5 @@
 import 'package:ninja_hex/ninja_hex.dart';
 
 void main() {
-  print(hexView(5, List<int>.generate(125, (i) => i + 5)));
+  print(hexView(5, List<int>.generate(125, (i) => i + 5), printAscii: true));
 }
